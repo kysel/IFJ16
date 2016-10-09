d38b8b91c789dc58c09c53f0f10931e336a504e9
viktorkovarik <viktorkovarik@MacBook-Pro-3.local>
2016-10-09 19:17:13 +0200

    initial commit

---
+++ src/ial.c
@@ -0,0 +1,8 @@
+/** Interpretr jazyka IFJ16
+* @file ial.c
+* @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
+* @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
+* @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+*/
