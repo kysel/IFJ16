0e1623541cb2ec610cf45dbb767599360f609443
viktorkovarik <viktorkovarik@MacBook-Pro-3.local>
2016-10-09 21:34:38 +0200

    declaration of find function

--- src/ial.c
+++ src/ial.c
@@ -6,3 +6,8 @@
 * @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
 * @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
 */
+
+int find(char* s, char* search) {
+// https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+// http://www.ms.mff.cuni.cz/~kopecky/vyuka/dis/02/dis02_v3.html
+}
