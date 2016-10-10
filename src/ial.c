775cdae94cba0c1e3c62912ad5e8a74bb015890d
Jiří Kyzlink <jkyzlink@gmail.com>
2016-10-11 00:44:19 +0200

    Added symbol table add; added windows project for cross platform testing

--- src/ial.c
+++ src/ial.c
@@ -3,11 +3,52 @@
 * @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
 * @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
 * @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
-* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
 * @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
 */
+
+#include "ial.h"
+#include "return_codes.h"
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int find(char* s, char* search) {
 // https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
 // http://www.ms.mff.cuni.cz/~kopecky/vyuka/dis/02/dis02_v3.html
 }
+
+symbol_tree_leaf_t* add_to_symbol_tree(symbol_tree_leaf_t* root, const char* key) {
+	assert(root);
+	int balance = strcmp(root->key, key);
+	if (balance == 0)
+		return root;
+
+	symbol_tree_leaf_t** newLeaf = NULL;
+	if (balance > 0) {
+		if (root->left != NULL)
+			return add_to_symbol_tree(root->left, key);
+		*newLeaf = root->left;
+	}
+	if (balance < 0) {
+		if (root->right != NULL)
+			return add_to_symbol_tree(root->right, key);
+		*newLeaf = root->left;
+	}
+
+	if (!newLeaf) {
+		*newLeaf = malloc(sizeof(symbol_tree_leaf_t));
+		if (!*newLeaf)
+			exit(internal_error);
+		(*newLeaf)->left = NULL;
+		(*newLeaf)->right = NULL;
+		(*newLeaf)->key = key;
+		return *newLeaf;
+	}
+	//this should NOT be reached!
+	assert(false);
+	return NULL;
+}
+
+
---
+++ src/ial.h
@@ -0,0 +1,43 @@
+﻿/** Interpretr jazyka IFJ16
+* @file ial.h
+* @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
+* @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
+* @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
+*/
+
+#ifndef IAL_H_
+#define IAL_H_
+
+#include <stdbool.h>
+
+typedef enum {
+	int_literal,
+	double_literal,
+	bool_literal,
+	string_literal
+}symbol_type_t;
+
+typedef struct value_type {
+	symbol_type_t type;
+	union {
+		int int_val;
+		double double_val;
+		bool bool_val;
+		char* string_val;
+	};
+}value_type_t;
+
+typedef struct symbol_tree_leaf {
+	const char* key;
+	int id;
+	/**
+	 * @note value_type'll used in interpret
+	 */
+	//value_type_t value;
+	struct symbol_tree_leaf* left;
+	struct symbol_tree_leaf* right;
+}symbol_tree_leaf_t;
+
+#endif //IAL_H_
