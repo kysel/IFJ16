68839789cd2fb6e04274b96ba776cd08da562f37
Jiří Kyzlink <jkyzlink@gmail.com>
2016-10-11 00:32:51 +0200

    Fixed authors

--- src/return_codes.h
+++ src/return_codes.h
@@ -3,6 +3,7 @@
 * @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
 * @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
 * @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
 * @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
 */
 
