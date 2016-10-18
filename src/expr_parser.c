79dfa7856895ec79e210295a9cd6dae516bf9742
Juraj Kubiš <jurkokubis@gmail.com>
2016-10-19 01:07:38 +0200

    working precedence tab, new functions token2TabIndex

--- src/expr_parser.c
+++ src/expr_parser.c
@@ -15,7 +15,7 @@ void stackInit (t_Stack* s) {
    s->arr = NULL; 
    s->stack_size = 0;
    s->top_element = -1;
-   s->top_terminal = -1;
+   s->top_token = -1;
 }
 
 int stackEmpty (const t_Stack* s) {
@@ -46,13 +46,13 @@ int findTopTerminal (t_Stack* s) {
 }
 
 void stackSetStopBit (t_Stack* s) {
-   s->arr[s->top_terminal].stop_bit = 1;
+   s->arr[s->top_token].stop_bit = 1;
 }
 
 void stackPop (t_Stack* s) {
    if (!stackEmpty(s)){
       s->top_element -= 1;
-      s->top_terminal = findTopTerminal(s);  
+      s->top_token = findTopTerminal(s);  
    }
 }
 
@@ -67,8 +67,32 @@ void stackPush (t_Stack* s, t_Element_Type type, void * address) {
    s->arr[s->top_element].address = address;
    s->arr[s->top_element].stop_bit = 0;
    if (type == EOS || type == TOKEN )
-      s->top_terminal = s->top_element;
+      s->top_token = s->top_element;
      
+}
+
+//
+int token2TabIndex(Ttoken * token) {
+   //Jediný "token", ktorý má adresu nula je EOS - End of Stack - Dno zásobníka
+   if (token == NULL)
+      return 13;
+   switch(token->type) {
+      case T_ADD: return 0;
+      case T_SUB: return 1;
+      case T_MUL: return 2;
+      case T_DIV: return 3;
+      case T_LOWER: return 4;
+      case T_GREATER: return 5;
+      case T_LOWER_EQUAL: return 6;
+      case T_GREATER_EQUAL: return 7;
+      case T_BOOL_EQUAL: return 8;
+      case T_NOT_EQUAL: return 9;
+      case T_BRACKET_LROUND: return 10;
+      case T_BRACKET_RROUND: return 11;
+      case T_ID: return 12;
+      case T_EOF: case T_COMMA: return 13;
+      default: return -1;//TODO error;
+   }
 }
 
 /***
@@ -80,13 +104,39 @@ void stackPush (t_Stack* s, t_Element_Type type, void * address) {
  * 
  */
 
-/*TODO
- node * parseExppression() {
+void parseExppression(FILE *file) {
+   t_Stack* stack = gc_alloc(sizeof(t_Stack));
+   stackInit(stack);
+   stackPush(stack, EOS, NULL);
+
+   Ttoken *a = stack->arr[stack->top_token].address;
+   Ttoken *b = get_token(file);
+   
    do {
-      switch (preccedense_tab[][])
-         case "=": break;   
-         case "<": break;
-         case ">": break;
-         default: break;
-   } while ();
-}*/
+      switch(precedence_tab[token2TabIndex(a)][token2TabIndex(b)]){
+         case 'E':
+            stackPush(stack, TOKEN, b);
+            b = get_token(file);
+            break;
+         
+         case 'L': 
+            stackSetStopBit(stack);
+            stackPush(stack, TOKEN, b);
+            b = get_token(file);
+            break;
+         
+         case 'M':
+            /* TODO 
+            stackApplyRule(s); 
+            */
+            break;
+         
+         default:
+            //TODO ERROR
+            break;   
+         }
+      
+      //Aktualizuje hodnotu top_token
+      a = stack->arr[stack->top_token].address;
+   } while (a != NULL || /*TODO*/b->type != EOF);
+}
