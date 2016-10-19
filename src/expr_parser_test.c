2c4ccbd21242a6b664dae5b4fee1699b1a22dc80
Juraj Kubis <liberouter@homeproj.cesnet.cz>
2016-10-20 00:38:37 +0200

    new test file

---
+++ src/expr_parser_test.c
@@ -0,0 +1,21 @@
+/** Interpretr jazyka IFJ16
+* @file expr_parser_test.c
+* @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
+* @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
+* @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
+*/
+
+#include "gc.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+   gc_init();
+   FILE *file = fopen(argv[1], "r");
+   parseExppression(file);
+   /**/
+   printf("ahoj\n");
+   return 1;   
+}
