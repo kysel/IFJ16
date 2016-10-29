f5ee1b6535daa38cdc07dc60506af0c652ff0648
Jiří Kyzlink <jkyzlink@gmail.com>
2016-10-29 18:46:50 +0200

    Added data_type, include stdbool

---
+++ src/ial.h
@@ -0,0 +1,53 @@
+﻿/** Interpretr jazyka IFJ16
+* @file ial.h
+* @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
+* @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
+* @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
+*/
+
+#ifndef IAL_H_
+#define IAL_H_
+
+typedef enum {
+	void_t,
+	int_t,
+	double_t,
+	bool_t,
+	string_t
+}Data_type;
+
+
+/*typedef struct Value_type_s {
+	Data_type type;
+	union {
+		int int_val;
+		double double_val;
+		bool bool_val;
+		char* string_val;
+	};
+}Value_type;*/
+
+typedef struct Symbol_tree_leaf_s {
+	const char* key;
+	int id;
+	Data_type value;
+	/**
+	 * @note Value_type_s'll used in interpret
+	 */
+	//Value_type value;
+	struct Symbol_tree_leaf_s* left;
+	struct Symbol_tree_leaf_s* right;
+}Symbol_tree_leaf;
+
+typedef struct {
+	Symbol_tree_leaf* root;
+	int nextId;
+}Symbol_tree;
+
+Symbol_tree symbol_tree_new();
+Symbol_tree_leaf* add_symbol(Symbol_tree* root, const char* key);
+Symbol_tree_leaf* get_symbol_by_key(Symbol_tree* root, const char* key);
+
+#endif //IAL_H_
