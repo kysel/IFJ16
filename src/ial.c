f09d4ecaf16bd780bdc8f410e537debba1ca7f60
oscrin <jn.kubica@gmail.com>
2016-10-30 21:38:55 +0100

    Correction of removed part of file.

--- src/ial.c
+++ src/ial.c
@@ -18,6 +18,33 @@
 // http://www.ms.mff.cuni.cz/~kopecky/vyuka/dis/02/dis02_v3.html
 //}
 
+char* sort(char* s) {      // razeni se snizujicim se prirustkem - Shell Sort
+   int n = strlen(s) - 1;  // spocitani poctu prvku (bez koncoveho znaku "\0")
+   int step = n / 2;       // jako prvni krok je bran pocet prvku lomen dvema (dle konvence)
+   int i;                  // pomocna promenna k prochazeni stringu od prvniho k poslednimu prvku
+   int j;                  // pomocna promenna k prochazeni paralelnich n-tic
+   char h;                 // pomocna promenna pro prohozeni dvou hodnot
+
+   while (step > 0) {            // dokud je krok vetsi jako nula
+      for(i = 0; i < n; i++) {   // cykly pro generovani paralelnich n-tic
+      //   printf("step=%d\n", step);
+      //   printf("i=%d\n", i);
+         j = i - step + 1;
+      //   printf("j1=%d\n", j);       
+         while (j >= 0 && s[j] > s[j + step]) { // prochazeni paralelnich n-tic
+      //      printf("j2=%d\n", j);
+            h = s[j];
+            s[j] = s[j + step];
+            s[j + step] = h;     // prohozeni dvojic na indexech rozdilnych o krok
+            j = j - step;        // snizeni indexu o krok
+         }
+      }
+      step = step / 2;           // redukce kroku dvema
+   }
+
+   return s;        
+}
+
 symbol_tree_t symbol_tree_new() {
 	return (symbol_tree_t) { .root = NULL, .nextId = 0 };
 }
