44863515295d25ca72a5fffe8b10c5a5353237e1
oscrin <jn.kubica@gmail.com>
2016-11-06 13:57:33 +0100

    Initial commit

---
+++ src/buildIn.h
@@ -0,0 +1,29 @@
+/** Interpretr jazyka IFJ16
+* @file buildIn.h
+* @author Kyzlink Jiří <xkyzli02@stud.fit.vutbr.cz>
+* @author Kubiš Juraj <xkubis15@stud.fit.vutbr.cz>
+* @author Korček Juraj <xkorce01@stud.fit.vutbr.cz>
+* @author Kubica Jan <xkubic39@stud.fit.vutbr.cz>
+* @author Kovařík Viktor <xkovar77@stud.fit.vutbr.cz>
+*/
+
+#include <stdio.h>
+
+// https://wis.fit.vutbr.cz/FIT/st/course-files-st.php/course/IFJ-IT/projects/ifj2016.pdf 
+// sekce 6
+
+// int readInt ();
+
+// double readDouble ();
+
+// String readString ();
+
+// void print ( term_nebo_konkatenace );
+
+// int length(String s);
+
+// String substr(String s, int i, int n);
+
+// int compare(String s1, String s2);
+
+
