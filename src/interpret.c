03f0a9d5507f3ce20064b0ae58df28ad9812591d
Jiří Kyzlink <jkyzlink@gmail.com>
2016-12-05 19:03:15 +0100

    Another fix

--- src/interpret.c
+++ src/interpret.c
@@ -200,7 +200,7 @@ Return_value eval_func(Inter_ctx* ctx, FunctionCall* fCall) {
     ctx->loc_stack = oldStack;
     if (f->type == user) {
         if (f->return_type == void_t) {
-            ret.val.type = void_t;
+            ret.val.init = false;
             return (Return_value) { .val = ret.val, .returned = true };
         }
         return (Return_value) { .val = cast(ret.val, f->return_type, false), .returned = true };
